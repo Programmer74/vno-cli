0d131882b06d33604fcb5b11d0713821b3c790d0
Nikita Tomilov <programmer174@icloud.com>
2018-04-12 09:13:43 +0300

    rest: dummy example for rapidjson

--- src/rest.cpp
+++ src/rest.cpp
@@ -13,8 +13,11 @@
 #include <curlpp/Exception.hpp>
 #include <curlpp/Infos.hpp>
 
+#include "document.h"
+
 using namespace std;
 using namespace curlpp::options;
+using namespace rapidjson;
 
 void rest::do_rest(string url) {
     cout << "Rest called" << endl;
@@ -32,18 +35,36 @@ void rest::do_rest(string url) {
         
         // Set where to write answer to
         std::stringstream result;
-
         myRequest.setOpt(cURLpp::Options::WriteStream(&result));
+        
+        // Set username/password
+        myRequest.setOpt(new curlpp::options::UserPwd("user:pwd"));
         
         // Send request and get a result.
         // By default the result goes to standard output.
         myRequest.perform();
         
-        //cout << result.str() << endl;
-        
         std::cout << "Response code: " 
 			<< curlpp::infos::ResponseCode::get(myRequest) 
 			<< std::endl;
+			
+        std::cout << "=====================" << endl;
+        Document document;
+        document.Parse(result.str().c_str());
+        
+        if (document.HasMember("error")) {
+            cerr << "Error occured: " << endl;
+            
+            assert(document.HasMember("status"));
+            assert(document["status"].IsInt());
+            cerr << "Status: " << document["status"].GetInt() << endl;
+            
+            assert(document.HasMember("message"));
+            assert(document["message"].IsString());
+            cerr << "Message: " << document["message"].GetString() << endl;
+        } else {        
+            cout << result.str() << endl;
+        }
     }
     
     catch(curlpp::RuntimeError & e)
@@ -58,3 +79,4 @@ void rest::do_rest(string url) {
 
     cout << "Rest done" << endl;
 }
+    
