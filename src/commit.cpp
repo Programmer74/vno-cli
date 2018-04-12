db54474456250036bbd664e167dfcf8fd77efd09
Nikita Tomilov <programmer174@icloud.com>
2018-04-12 20:09:58 +0300

    commit: now commit generates json for server side

--- src/commit.cpp
+++ src/commit.cpp
@@ -10,7 +10,12 @@
 #include "paths.h"
 #include "status.h"
 
+#include "document.h"
+#include "writer.h"
+#include "stringbuffer.h"
+
 using namespace std;
+using namespace rapidjson;
 
 void commit::do_commit(string commit_message) {
     cout << "Commit called" << endl;
@@ -23,6 +28,13 @@ void commit::do_commit(string commit_message) {
     ofstream commitfile;
     string commitfile_path = TMP_COMMIT_FILE;
     commitfile.open(commitfile_path);
+    
+    // document is the root of a json message
+	rapidjson::Document document;
+ 	// define the document as an object rather than an array
+	document.SetObject();
+	// must pass an allocator when the object may need to allocate memory
+	rapidjson::Document::AllocatorType& allocator = document.GetAllocator();
         
     string parent;
     ifstream parentcfile(LAST_COMMIT_ID_FILE);
@@ -30,19 +42,28 @@ void commit::do_commit(string commit_message) {
     
     //storing the new commit's parent commit hash
     commitfile << parent << endl;
+    document.AddMember(rapidjson::Value("parent_commit_id", allocator), rapidjson::Value(parent.c_str(), allocator), allocator);
     
     //storing the commit's message
     commitfile << commit_message << endl;
+    document.AddMember(rapidjson::Value("commit_message", allocator), rapidjson::Value(commit_message.c_str(), allocator), allocator);
     
     //storing the commit's author
-    commitfile << utils::read_line_from_file(utils::home_dir + SETTINGS_FILE, 0) << endl;
+    string commit_author = utils::read_line_from_file(utils::home_dir + SETTINGS_FILE, 0);
+    commitfile << commit_author << endl;
+    document.AddMember(rapidjson::Value("author_id", allocator), rapidjson::Value(commit_author.c_str(), allocator), allocator);
     
     //storing the timestamp
     std::time_t result = std::time(nullptr);
     commitfile << result << endl;
+    document.AddMember(rapidjson::Value("timestamp", allocator), rapidjson::Value(to_string(result).c_str(), allocator), allocator);
+    
+    //setup the array for json
+    rapidjson::Value array(rapidjson::kArrayType);\
     
     //copying files from tree to blob dir
     //storing working tree: 1st line for path, 2nd line for blob path
+    //and also storing them to json lol
     vector<string> fnames = utils::parse_file_tree(WORKING_DIR);
     for (string fname : fnames) {
         std::string hash = utils::hashfile(fname);
@@ -50,7 +71,16 @@ void commit::do_commit(string commit_message) {
         utils::copy_file(fname, BLOBS_DIR + hash);
         commitfile << fname << endl;
         commitfile << hash << endl;
+        
+        rapidjson::Value cobj(rapidjson::kObjectType);
+        cobj.AddMember(rapidjson::Value("full_path", allocator), rapidjson::Value(fname.c_str(), allocator), allocator);
+        cobj.AddMember(rapidjson::Value("blob_id", allocator), rapidjson::Value(hash.c_str(), allocator), allocator);
+    
+        array.PushBack(cobj, allocator);
     }
+    
+    document.AddMember(rapidjson::Value("working_tree", allocator), array, allocator);
+    
     commitfile.close();
     std::string commithash = utils::hashfile(commitfile_path);
     utils::move_file(commitfile_path, COMMITS_DIR + commithash);
@@ -65,4 +95,10 @@ void commit::do_commit(string commit_message) {
     utils::write_to_file(BRANCHES_DIR + current_branch, commithash);
     
     cout << "Commit done; hash : " << commithash << endl;
+    cout << "JSON:" << endl;
+    StringBuffer strbuf;
+	Writer<StringBuffer> writer(strbuf);
+	document.Accept(writer);
+
+	std::cout << strbuf.GetString() << std::endl;
 }
